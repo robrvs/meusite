c5aa5804d07dfac4fd5b27abe9ed1f23c37d8223
robrvs <39736736+robrvs@users.noreply.github.com>
2026-05-20 00:18:48 -0400

    Form_Login_20.05.2026

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
-def login_view(request):
-    return render(request, 'usuarios/login.html')
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django  
+
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'usuarios/login.html')
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        user = authenticate(request, username=email, password=senha)
+        if user is not None:
+            login_django(request, user)  
+            return HttpResponse('Login autenticado com sucesso!')
+        else:
+            return HttpResponse('e-mail ou senha inválidos.')
