cb8d6c900154217a336f505c9d1a580dd1828231
robrvs <39736736+robrvs@users.noreply.github.com>
2026-06-06 02:46:54 -0400

    06.06.2026 - 02:46

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,4 +4,13 @@ from . import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('cadastro/', views.cadastro, name='cadastro'),
-]
+    path('home/', views.home, name='home'),
+    path('lancar/', views.lancarNotas, name='lancar'),
+    path('alterar/', views.alterar, name='alterar'),
+    path('visualizar/', views.visualizar, name='visualizar'),
+    path('logout/', views.logout_view, name='logout'),
+    path('excluir_verificacao/<int:pk>/', views.excluir_verificacao, name='excluir_verificacao'),
+   path('excluir/<int:pk>/', views.excluir, name='excluir'),
+   path('editar_verificacao/<int:pk>/', views.editar_verificacao, name='editar_verificacao'),
+   path('editar/<int:pk>/', views.editar, name='editar'),
+]   
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,27 +1,31 @@
+import email
+
 from django.shortcuts import render, redirect
 from django.http.response import HttpResponse
 
 from django.contrib.auth.models import User
+from django.contrib.auth import logout
 from django.contrib.auth import authenticate
-from django.contrib.auth import login as login_django
+from django.contrib.auth import login as login_django, logout as logout_django
 
+from usuarios.models import Nota
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def login_view(request):
     if request.method == 'GET':
         return render(request, 'usuarios/login.html')
-
-    if request.method == 'POST':
-        username = request.POST['username']
+    else:
+        email = request.POST['email']
         senha = request.POST['senha']
 
-        user = authenticate(request, username=username, password=senha)
+        user = authenticate(request, username=email, password=senha)
 
         if user is not None:
             login_django(request, user)
-            return HttpResponse('Login autenticado com sucesso!')
+            return render(request, 'usuarios/home.html')
         else:
             return HttpResponse('e-mail ou senha inválidos.')
-
 
 def cadastro(request):
     if request.method == 'GET':
@@ -49,3 +53,103 @@ def cadastro(request):
             user.save()
 
             return HttpResponse('Usuário cadastrado com sucesso!')
+        
+def home(request):
+    if request.user.is_authenticated:
+        return render(request, 'usuarios/home.html')
+    else:
+        return HttpResponse('Faça o login para acessar!')
+
+def lancarNotas(request):
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            return render(request, 'usuarios/lancarNotas.html')
+        else:
+            return HttpResponse('Faça o login para acessar!')
+    else: # POST
+        nota = Nota()
+        nota.nome_aluno = request.user.first_name
+        nota.disciplina = request.POST.get('disciplina')
+        nota.nota_atividades = request.POST.get('nota_atividades')
+        nota.nota_trabalho = request.POST.get('nota_trabalho')
+        nota.nota_prova = request.POST.get('nota_prova')
+        nota.media = int(nota.nota_atividades) + int(nota.nota_trabalho) + int(nota.nota_prova) // 3
+        nota_verificada = Nota.objects.filter(disciplina=nota.disciplina).first()
+        if nota_verificada:
+            return HttpResponse('Disciplina já possui notas cadastradas!')
+        else:
+            nota.save()
+            return render(request, 'usuarios/home.html')
+
+def alterar(request):
+    if request.user.is_authenticated:
+        lista_notas = Nota.objects.all()
+        dicionario_notas = {'lista_notas': lista_notas}
+        return render(request, 'usuarios/alterarNotas.html', dicionario_notas)
+    else:
+        return HttpResponse('Faça o login para acessar!')
+
+def visualizar(request):
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            lista_Notas = Nota.objects.all()
+            dicionario_notas = {'lista_notas': lista_Notas}
+            return render(request, 'usuarios/visualizarNotas.html', dicionario_notas)
+        else:
+            return HttpResponse('Faça o login para acessar!')
+    else:
+        disciplina = request.POST.get('disciplina')
+        if disciplina == 'Todas as disciplinas':
+            lista_Notas = Nota.objects.all()
+        else:
+            lista_Notas = Nota.objects.filter(disciplina=disciplina)
+        dicionario_notas = {'lista_notas': lista_Notas}
+        return render(request, 'usuarios/visualizarNotas.html', dicionario_notas)
+    
+def editar(request, pk):
+        if request.method == "POST":
+            if request.user.is_authenticated:
+                nome_aluno = request.user.first_name
+                disciplina = request.POST.get('disciplina')
+                nota_atividades = request.POST.get('nota_atividades')
+                nota_trabalho = request.POST.get('nota_trabalho')
+                nota_prova = request.POST.get('nota_prova')
+                media = int(nota_atividades) + int(nota_trabalho) + int(nota_prova)
+                Nota.objects.filter(pk=pk).update(nome_aluno=nome_aluno, disciplina=disciplina, nota_atividades=nota_atividades,nota_trabalho=nota_trabalho, nota_prova=nota_prova, media=media)
+                return HttpResponseRedirect(reverse('alterar'))
+    
+def editar_verificacao(request, pk):
+        if request.method == "GET":
+            if request.user.is_authenticated:
+                lista_notas = Nota.objects.get(pk=pk)
+                dicionario_notas = {'lista_notas': lista_notas}
+                return render(request, 'usuarios/editar.html', dicionario_notas)
+            else:
+                return HttpResponse('Faça o login para acessar!')    
+    
+def excluir_verificacao(request, pk):
+        if request.method == "GET":
+            if request.user.is_authenticated:
+                lista_notas = Nota.objects.get(pk=pk)
+                dicionario_notas = {'lista_notas': lista_notas}
+                return render(request, 'usuarios/excluir.html', dicionario_notas)
+            else:
+                return HttpResponse('Faça o login para acessar!')
+            
+def excluir(request, pk):
+        if request.method == "GET":
+            if request.user.is_authenticated:
+                disciplina_selecionada = Nota.objects.get(pk=pk)
+                disciplina_selecionada.delete()
+                return HttpResponseRedirect(reverse('alterar'))
+            else:
+                return HttpResponse('Faça o login para acessar!')
+
+def logout_view(request):
+    if request.user.is_authenticated:
+        logout_django(request)
+        return render(request, 'usuarios/login.html')
+    else:
+        return HttpResponse('Você não acessou sua conta ainda!!')
+    
+
